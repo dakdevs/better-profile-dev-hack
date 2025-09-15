46d11931a41f91dd83b22f97d3ddffd5cfcf85be
sumiran35 <128114569+sumiran35@users.noreply.github.com>
2025-09-15 08:47:09 -0700

    package issue solved

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -3,12 +3,7 @@ export * from './accounts'
 export * from './sessions'
 export * from './verifications'
 export * from './interview-messages'
-<<<<<<< HEAD
 export * from './embeddings'
 export * from './recruiter'
 export * from './jobs'
 export * from './user-skills'
-=======
-export * from './recruit-settings'
-export * from './jobs'
->>>>>>> production
